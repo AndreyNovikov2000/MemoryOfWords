7fd8b5a991d42d2b33ea4822b0899828a3e0320c
Novikov Andrey <novikovandrey2000@gmail.com>
2020-12-12 15:19:48 +0600

    Initial Commit

---
+++ MemoryOfWords/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemoryOfWords
+//
+//  Created by Andrey Novikov on 12/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
