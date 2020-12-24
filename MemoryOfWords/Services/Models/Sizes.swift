1f24e166e0777e6e6e300d7b5f5008000b18d1db
Novikov Andrey <novikovandrey2000@gmail.com>
2020-12-24 22:34:59 +0600

    Sizes

---
+++ MemoryOfWords/Services/Models/Sizes.swift
@@ -0,0 +1,13 @@
+//
+//  Sizes.swift
+//  MemoryOfWords
+//
+//  Created by AlemTime.
+//
+
+import UIKit
+
+struct Sizes {
+    static let width = UIScreen.main.bounds.width / 2 - (2 * 16)
+    static let height = width
+}
